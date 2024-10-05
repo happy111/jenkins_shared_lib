f1d4a21c8024f9efa0ed3f052f01bf4a3b108e09
Umesh Samal <umeshsamal3@gmail.com>
2024-10-05 08:09:17 +0530

    Create docker_login.groovy

---
+++ vars/docker_login.groovy
@@ -0,0 +1,6 @@
+def call(String Project, String ImageTag, String dockerhubuser){
+  withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
+
+}
